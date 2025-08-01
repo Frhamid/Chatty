9e9ef8981c686e5f81382446eb35b29284b35f5e
Frhamid <frhamid2000@gmail.com>
2025-08-01 05:05:28 -0700

    Enpoints created for friends and friend requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 // import { connectDB } from "./lib/db.js";
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
