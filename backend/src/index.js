43e793df867ce1f2b6d93d1fa6974a5101b7ca72
Frhamid <frhamid2000@gmail.com>
2025-08-30 12:45:06 -0700

    deployment fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,9 +33,12 @@ const frontendPath = path.join(__dirname, "../../frontend/dist");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
+  // app.get("/*", (req, res) => {
+  //   res.sendFile(path.join(frontendPath, "index.html"));
+  // });
 }
 
 // app.listen(PORT, () => {
