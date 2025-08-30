d8660a9e8927ddde064ec42db75b8d692410be81
Frhamid <frhamid2000@gmail.com>
2025-08-30 13:07:55 -0700

    deployment fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,12 +30,12 @@ app.use("/api/chat", chatRoutes);
 
 const frontendPath = path.join(__dirname, "../../frontend/dist");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(frontendPath));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(frontendPath));
 
-  app.use((req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
+//   app.use((req, res) => {
+//     res.sendFile(path.join(frontendPath, "index.html"));
+//   });
   // app.get("/*", (req, res) => {
   //   res.sendFile(path.join(frontendPath, "index.html"));
   // });
