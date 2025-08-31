0de5397bad8bf653e23b37e1f27f68a04a4a3874
Frhamid <frhamid2000@gmail.com>
2025-08-31 05:10:43 -0700

    upddated my vercel .json

--- frontend/src/lib/Axios.js
+++ frontend/src/lib/Axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.PROD_API_BASE_URL; // define this in your production env
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
